8b553d6c57e5a81b102227a136bf3d95c845503a
MaxianEdison <MaximilianEdison@gmail.com>
2024-01-06 08:27:49 +0330

    make template for using cf-tool for cp

---
+++ w/competition_programming/tools/cf.cpp
@@ -0,0 +1,16 @@
+/*
+*    @Author:   MaximilianEdison 
+*    @Date:  $%Y%$-$%M%$-$%D%$ $%h%$:$%m%$:$%s%$
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+#ifdef DEBUG
+    std::cout << std::unitbuf; // enable automatic flushing for std::cout (use for debugging only)
+#endif
+
+    return 0;
+}
