f4d1455cbae724e995ba93d238baca0ae9a71403
MaxianEdison <MaximilianEdison@gmail.com>
2024-02-04 19:19:28 +0330

    convert c template to cpp

---
+++ w/competition_programming/tools/cf.cpp
@@ -0,0 +1,15 @@
+/*
+*    @Author:   MaximilianEdison 
+*    @Date:  $%Y%$-$%M%$-$%D%$ $%h%$:$%m%$:$%s%$
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+     
+    return 0;
+}
+/*
+
+*/
